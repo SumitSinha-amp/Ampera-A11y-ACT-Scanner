e4c912344fe7f01144707a93b59250e111a4cd9b
Sumit Sinha <sumit.sinha@amperatech.ai>
2026-04-21 20:29:11 +0530

    Update startup.sh for logging

--- .azure/startup.sh
+++ .azure/startup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
+echo "Starting app..."
+pwd
+ls -la
+
 apt-get update -qq
 apt-get install -y -qq chromium
 
+echo "Running node app..."
 node /home/site/wwwroot/artifacts/api-server/dist/index.mjs
